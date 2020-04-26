e826c1b2659abb00655aeb56bba2c5019f36c4dc
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-04-26 07:23:58 -0700

    commit 5

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+const res = await fetch('https://randomuser.me/api');
+const data = await res.json();
+
+const user = data.results[0];
+
+const newUser = {
+    name: '${user.name.first} ${user.name.last}',
+    money: Math.floor(Math.random() * 1000000)
+}
+addData(newUser);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+    data.push(obj);
+}
