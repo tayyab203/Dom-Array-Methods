49c70cb0f82eee16be79f5f3a17e7ee0182b836e
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-04-26 21:52:32 -0700

    commit 8

--- script.js
+++ script.js
@@ -69,3 +69,29 @@ function calculateWealth() {
     updateDOM();
   }
   
+// Update DOM
+function updateDOM(providedData = data) {
+    // Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  
+    providedData.forEach(item => {
+      const element = document.createElement('div');
+      element.classList.add('person');
+      element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+        item.money
+      )}`;
+      main.appendChild(element);
+    });
+  }
+  
+  // Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+  function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  }
+  
+  // Event listeners
+  addUserBtn.addEventListener('click', getRandomUser);
+  doubleBtn.addEventListener('click', doubleMoney);
+  sortBtn.addEventListener('click', sortByRichest);
+  showMillionairesBtn.addEventListener('click', showMillionaires);
+  calculateWealthBtn.addEventListener('click', calculateWealth);
